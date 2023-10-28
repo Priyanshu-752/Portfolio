821de2bf205f5d5719624d564184d555f7fab888
PRIYANSHU NEGI <priyanshunegi752@gmail.com>
2023-10-28 19:45:24 +0530

    Img

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,8 +1,7 @@
 export const Bio = {
   name: "Priyanshu Negi",
   roles: [
-    "Full Stack Developer",
-    "App Developer",
+    "Frontend Developer",
     "UI/UX Designer",
     "Programmer",
   ],
@@ -78,24 +77,6 @@ export const skills = [
         image:
           "https://media.licdn.com/dms/image/D4D22AQFG4EvNQbeFKg/feedshare-shrink_800/0/1694803059847?e=1697673600&v=beta&t=mqNSepqpZprIPYaxxafp0RmdGIRBN56BolkWLhk2CwA", 
         },
-      // {
-      //   name: "Graph Ql",
-      //   image: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
-      // },
-      {
-        name: "Python",
-        image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
-      },
-      {
-        name: "MySQL",
-        image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
-      },
-      {
-        name: "Prisma",
-        image: "https://media.licdn.com/dms/image/D4D22AQGXbTzMZSMJvw/feedshare-shrink_800/0/1694803059822?e=1697673600&v=beta&t=a-cimT6SODt80TJFRO-4pSCm0KGfUbvylyrN-PzPvpM" ,
-           },
       {
         name: "MongoDB",
         image:
@@ -125,16 +106,6 @@ export const skills = [
         image:
           "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAS1BMVEU0SMUyRsQwRcQ5TcczR8VZadCNmN+vuOnFy++AjdtNX8zx8vv////o6vn7/P7i5fdBVMlicdLY3PTP1PKZo+K6wextfNakreV2g9gfSrnCAAABI0lEQVR4Ad3Rx4EDIQwFUIRE1pDB03+lO2Gd7Qb8L6RHFr8XkAKRJH0bFkoba43z9JGAN4HDskROOn8QUGpsTnlfeorjXUhVQ88SiCT4wa8ChB9RC/pv5a3xBEiZhQfSzfuUvLwTmim2i4KHFTubmeHaUktwGQDpDsoSo71OwREdECizd8CpUE0T6ylABYtEqnJTROXoJAESdRy4A7mylgINM19wLnUXmTaENhxaOnZSwBo5riUxNw/u2F5qXuUOZuwg5A6mOoALXBXdAPmlZjoBVuaRB/PxaiYWKfZcuOMJ8g6EZg4KaA3tfHDyLfYHQAcgHcKE60u1IeYGHDZmi443iy2t94fLReSLtR1Xa4fyxlqN08PTd5JAREG45awCid/JH5X3D4qGe2LBAAAAAElFTkSuQmCC",
       },
-      // {
-      //   name: "XML",
-      //   image:
-      //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
-      // },
-      // {
-      //   name: "Android Studio",
-      //   image:
-      //     "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
-      // },
     ],
   },
   {
@@ -150,11 +121,6 @@ export const skills = [
         image:
           "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
       },
-      // {
-      //   name: "Docker",
-      //   image:
-      //     "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
-      // },
       {
         name: "Netlify",
         image:
@@ -185,24 +151,7 @@ export const skills = [
 ];
 
 export const experiences = [
-  {
-    id: 0,
-    img: "https://media.licdn.com/dms/image/C4D0BAQExP1F1c0WD4w/company-logo_200_200/0/1675191329016?e=1699488000&v=beta&t=ddjIA8UO62wBZhz2LVL7UAbJYEqCOXBN-uUoeVy_jJI",
-    role: "React-js Developer Intern",
-    company: "SYNC INTERN'S",
-    date: "July 2023 - Present",
-    desc: "Working on the frontend of the web application using ReactJS, Redux, and Tailwind Css. Deploying them in the cloud nservers as Versel and Netlify",
-    skills: [
-      "ReactJS",
-      "Redux",
-      "NodeJs",
-      "Tailwind Css",
-      "HTML",
-      "CSS",
-      "JavaScript",
-    ],
-    // doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
-  },
+
   {
     id: 1,
     img: "https://media.licdn.com/dms/image/C4D0BAQEm_0ntX9UAlg/company-logo_200_200/0/1672719888099?e=1699488000&v=beta&t=CDxq4U88gQwFMMrev3xDgGCIZvsGGQJhEgZEurSOSTo",
@@ -222,22 +171,7 @@ export const experiences = [
       "MongoDB",
     ],
   },
-  {
-    id: 2,
-    img: "https://media.licdn.com/dms/image/C4D0BAQHEfFRhrG3gxw/company-logo_100_100/0/1677433232801?e=1699488000&v=beta&t=hTnkyDJbbL5-2Y2LWOqvQ32Eo75UyQgDTbX1sx22rn8",
-    role: "Full-stack Developer Intern",
-    company: "Bharat Intern",
-    date: "June 2023 - Aug 2023",
-    desc: "As an Full-stack Developer developer at the Bharat Intern, I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with Bharat Intern, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
-    skills: [
-      "MERN Stack",
-      "Html",
-      "Css",
-      "WebRtc",
-      "Firebase", 
-      "Figma",
-    ],
-  },
+
   {
     id: 3,
     img: "https://media.licdn.com/dms/image/D4D0BAQHWecVdo6_CUw/company-logo_100_100/0/1690364566368?e=1699488000&v=beta&t=tzCUpVX-15yiTecvef9jh9Qbx9A8rGEnDyU2f0Vp2Kw",
@@ -267,12 +201,12 @@ export const education = [
     school: "Graphic Era Hill University, Haldwani",
     date: "Oct 2021 - July 2025",
     grade: "8.12 CGPA",
-    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Graphic Era Hill University, Haldwani. I have completed 4 semesters and have a CGPA of 8.12. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Software Development Cell at GEHU, where I am learning and working on exciting projects with a team of talented developers.",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Graphic Era Hill University, Haldwani. Currently in 5th semester. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web XR, and UI/UX, among others. I am also a member of the Software Development Cell at GEHU, where I am learning and working on exciting projects with a team of talented developers.",
     degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
   },
   {
     id: 1,
-    img:"https://media.licdn.com/dms/image/D4D22AQED9CC9_il8bA/feedshare-shrink_800/0/1694803265102?e=1697673600&v=beta&t=BYLvTZ1qkAkp_FUpnrU7La6gbd3Dx-YMtKZanuwjDXA",
+    img:"https://media.licdn.com/dms/image/D4D22AQED9CC9_il8bA/feedshare-shrink_800/0/1694803265102?e=1701302400&v=beta&t=ChzejOBfItGZsD7tGWBatUVyaW07jgFzZKpNm3EquVE",
     school: "St. Theresa Sr. Sec. School Kathgodam, Nainital",
     date: "Apr 2019 - Apr 2021",
     grade: "81.2%",
@@ -281,7 +215,7 @@ export const education = [
   },
   {
     id: 2,
-    img:"https://media.licdn.com/dms/image/D4D22AQED9CC9_il8bA/feedshare-shrink_800/0/1694803265102?e=1697673600&v=beta&t=BYLvTZ1qkAkp_FUpnrU7La6gbd3Dx-YMtKZanuwjDXA",
+    img:"https://media.licdn.com/dms/image/D4D22AQED9CC9_il8bA/feedshare-shrink_800/0/1694803265102?e=1701302400&v=beta&t=ChzejOBfItGZsD7tGWBatUVyaW07jgFzZKpNm3EquVE",
     school: "St. Theresa Sr. Sec. School Kathgodam, Nainital",
     date: "Apr 2017 - Apr 2019",
     grade: "86.3%",
@@ -297,7 +231,7 @@ export const projects = [
     date: "June 2023 - July 2023",
     description:
       "1st Internal Conference website.Me and my teammeats Mayank Bisht and Gayatri Mehta have worked on the website for School of Computing, Graphic Era Hill University (GEHU) Haldwani (U. K.), India is organizing an “International Conferences on Advanced Computing and Information Science (ICACIS-2023)” which was held on August 11-12th, 2023. It was a conference to write your paper and present it to be selected to public the paper , it was held on international level .",
-    image:"https://media.licdn.com/dms/image/D4D22AQGdW-VUtHts-g/feedshare-shrink_2048_1536/0/1694802746376?e=1697673600&v=beta&t=pCRhT8Wj_P6KOzi-iIHa_FipXbeC2NGshJ6FImdRNiE",
+    image:"https://media.licdn.com/dms/image/D4D22AQGdW-VUtHts-g/feedshare-shrink_2048_1536/0/1694802746376?e=1701302400&v=beta&t=zW_BOXGHe4AeL-jyvVW_aOdWBpq49npkvO4gFhdialM",
     tags: ["React Js", "Redux", "Modular Css", "Versel"],
     category: "web app",
     github: "https://github.com/Priyanshu-752/Dicct",
@@ -309,7 +243,7 @@ export const projects = [
     date: "Jul 2023 - Aug 2023",
     description:
       "Mumble is a video conferencing app in which user can make their personal rooms and can add their friends in it .It has video call , chat functonality , also shows number of participants and many more features.",
-    image:"https://media.licdn.com/dms/image/D4D22AQHg7vEJ5mtVXg/feedshare-shrink_2048_1536/0/1694802739115?e=1697673600&v=beta&t=SPW0v7j-Gjyeay_c_wilpVM1i_OdQJy_pgt8Sk6p1JE",
+    image:"https://media.licdn.com/dms/image/D4D22AQHg7vEJ5mtVXg/feedshare-shrink_2048_1536/0/1694802739115?e=1701302400&v=beta&t=8SoUCBFCurSr3rIhfZ5WJyjikzzWQZa1NqY7FHigcYE",
      tags: [
       "Versel",
       "WebRtc",
@@ -328,7 +262,7 @@ export const projects = [
     date: "Mar 2023 - Apr 2023",
     description:
       "It is a news app that provide latest news in front page. It also has search functionality i.e. user can search about the topic about which they want to know , categories section such as sports, politics, entertainment, health, etc.",
-    image:"https://media.licdn.com/dms/image/D4D22AQHSvYLQHDMSEA/feedshare-shrink_800/0/1694802739895?e=1697673600&v=beta&t=Y2CKVabnh9SB_Rt4P-TQy1CeYL8KJ6bHmoMrI4QSHVA",
+    image:"https://media.licdn.com/dms/image/D4D22AQHSvYLQHDMSEA/feedshare-shrink_2048_1536/0/1694802739886?e=1701302400&v=beta&t=0bdi-4Q98zjLyfbyRJCSVBnLw2pth0ODXnWMtpA22UE",
      tags: ["Next Js", "Typescript", "Tailwind Css", "News Api"],
     category: "web app",
     github: "https://github.com/Priyanshu-752/News__Jour",
@@ -340,7 +274,7 @@ export const projects = [
     date: "Mar 2023 - Mar 2023",
     description:
       "It's an resume building app. In this you can add title and can shift the position of the titles, It provides all nesscary information details. ",
-    image:"https://media.licdn.com/dms/image/D4D22AQGYtqh4D5T0DA/feedshare-shrink_800/0/1694802737790?e=1697673600&v=beta&t=iEevk3jC2dc6LJDLpYY_bLWKmKyLAykvghj1S60_7Ck",
+    image:"https://media.licdn.com/dms/image/D4D22AQGYtqh4D5T0DA/feedshare-shrink_2048_1536/0/1694802737783?e=1701302400&v=beta&t=-3Pli-Knf-mOZ197sK2LK3ZJk-Sn4yxvG_X3e2ga9po",
       tags: [
       "React Js",
       "Node Js",
@@ -351,116 +285,33 @@ export const projects = [
     github: "https://github.com/Priyanshu-752/Resume_buddy",
     webapp: "https://resume-buddy.vercel.app/",
   },
-  // {
-  //   id: 2,
-  //   title: "Brain Tumor Detection",
-  //   date: "Jan 2023 - Mar 2023",
-  //   description:
-  //     "Preprocessed and augmented the dataset to improve model accuracy, trained the model, created API using model and Python, and used React web app for the project's front end. Achievements: Achieved an accuracy of 99.2% to accurately detect brain tumors from medical images.",
-  //   image:
-  //     "https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
-  //   tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
-  //   category: "machine learning",
-  //   github: "https://github.com/rishavchanda/Brain-Tumor-Detection",
-  //   webapp: "https://resume-buddy.vercel.app/",
-  //   member: [
-  //     {
-  //       name: "Rishav Chanda",
-  //       img: "https://avatars.githubusercontent.com/u/64485885?v=4",
-  //       linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  //       github: "https://github.com/rishavchanda/",
-  //     },
-  //     {
-  //       name: "Upasana Chaudhuri",
-  //       img: "https://avatars.githubusercontent.com/u/100614635?v=4",
-  //       linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
-  //       github: "https://github.com/upasana0710",
-  //     },
-  //   ],
-  // },
+
   {
     id: 3,
     title: "Ycrypto",
     date: "Dec 2021 - Apr 2022",
     description:
       "Its a Crypto Maket website, that gives latest information about crypto market and Ups and Downs in the market.It provides all crypto coins details. I have implemented inline styling and crypto API , through this API i have fetched current prices and implemented coins prices in form of graph which show previous 24 hrs , 7 days, 1 year data.",
-    image:"https://media.licdn.com/dms/image/D4D22AQHRIfWObCdGXg/feedshare-shrink_800/0/1694802737894?e=1697673600&v=beta&t=j5585wTuH3A7TQxynY54gMIgJ8T8sxeAq-lz12iJOHw",
+    image:"https://media.licdn.com/dms/image/D4D22AQHRIfWObCdGXg/feedshare-shrink_2048_1536/0/1694802737948?e=1701302400&v=beta&t=wm4kIwSup6Gp5iaRoGPou2W4Nqh67XZxIn0Bbm6NfYQ",
     tags: ["React Js", "Chakra Ui", "JavaScript", "Crypto API"],
     category: "web app",
     github: "https://github.com/Priyanshu-752/Ycrypto",
     webapp: "https://ycrypto.vercel.app/",
   },
-  // {
-  //   id: 10,
-  //   title: "Job Finding App",
-  //   date: "Jun 2023 - Jul 2023",
-  //   description:
-  //     "A Job Finding App made with React Native, Axios. Users can search for any job coming from API and apply there.",
-  //   image:
-  //     "https://user-images.githubusercontent.com/64485885/255237090-cf798a2c-1b41-4bb7-b904-b5353a1f08e8.png",
-  //   tags: ["React Native", "JavaScript", "Axios"],
-  //   category: "android app",
-  //   github: "https://github.com/rishavchanda/Job-finder-App",
-  //   webapp: "https://github.com/rishavchanda/Job-finder-App",
-  // },
-  {
-    id: 4,
-    title: "Whatsapp Clone",
-    date: "Jul 2021",
-    description:
-      "A WhatsApp clone made with React JS, Firebase, and Material UI. It has Phone Authentication, Real-time Database. It has a chat room where users can chat with each other. It has a sidebar where users can see all the chat rooms and can create a new chat room. It has a login page where users can log in with their Google account.",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/whatsapp-clone-rishav.appspot.com/o/Screenshot%20(151).png?alt=media&token=48391593-1ef0-4a8c-a92a-eb82bdf38e89",
-    tags: ["React Js", "Firebase", "Firestore", "Node JS"],
-    category: "web app",
-    // github: "https://github.com/rishavchanda/Whatsapp-Clone-React-Js",
-    // webapp: "https://whatsapp-clone-rishav.web.app",
-  },
-  // {
-  //   id: 5,
-  //   title: "Todo Web App",
-  //   date: "Jun 2021",
-  //   description:
-  //     " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
-  //   image:
-  //     "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
-  //   tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
-  //   category: "web app",
-  //   github: "https://github.com/rishavchanda/Todo-Web-App",
-  //   webapp: "https://rishav-react-todo.netlify.app/",
-  // },
+
   {
     id: 6,
     title: "Nike 2.0",
     date: "Nov 2022",
     description:
       "Its an Upgraded verson of nike e-commerce website designed by me.",
-    image:"https://media.licdn.com/dms/image/D4D22AQGEPqzdA6SzUg/feedshare-shrink_800/0/1694802739913?e=1697673600&v=beta&t=IqOsdF8j39UpDyRde0YM_BAgUrgV6j3ebsHHFTyGJnI",
+    image:"https://media.licdn.com/dms/image/D4D22AQGEPqzdA6SzUg/feedshare-shrink_2048_1536/0/1694802739901?e=1701302400&v=beta&t=Nimksdlr8PxO_uXWTVW9mWQKQ637GrDyW2xPB9YKPUo",
     tags: ["React Js", "Redux", "Tailwind Css", "Versel"],
     category: "web app",
     github: "https://github.com/Priyanshu-752/Nike-2.0",
     webapp: "https://nike-2-0.vercel.app/",
   },
 
-  // {
-  //   id: 8,
-  //   title: "Face Recognition",
-  //   date: "Jan 2021",
-  //   description:
-  //     "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
-  //   image:
-  //     "https://camo.githubusercontent.com/a8b3e1ebf26d4c95f75bc6671189a3590eb67eb8332d7c7452492990e731efb9/68747470733a2f2f77617265686f7573652d63616d6f2e696e67726573732e636d68312e707366686f737465642e6f72672f356137383335396561323762643261633232336437656662306639303831306437373930383436312f363837343734373037333361326632663633366336663735363432653637363937343638373536323735373336353732363336663665373436353665373432653633366636643266363137333733363537343733326633383339333633363339333232663332333433343333333033333339333832663333333636363330363533333636333032643331333336333632326433313331363533373264333833323335333832643334363433303633333936333635333136353334333133393265363736393636",
-  //   tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
-  //   category: "machine learning",
-  //   github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
-  //   webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
-  // },
+
 ];
 
-// export const TimeLineData = [
-//   { year: 2017, text: "Started my journey" },
-//   { year: 2018, text: "Worked as a freelance developer" },
-//   { year: 2019, text: "Founded JavaScript Mastery" },
-//   { year: 2020, text: "Shared my projects with the world" },
-//   { year: 2021, text: "Started my own platform" },
-// ];
