a2f56dbc0031a96688becb6f515c6de3e653111e
Priyanshu Negi <priyanshunegi752@gmail.com>
2024-07-21 18:14:05 +0530

    RU2

--- src/data/constants.js
+++ src/data/constants.js
@@ -28,7 +28,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Priyanshu-752",
   resume:
-    "https://drive.google.com/file/d/10w66hvq5sJbdmNE6tmxZW-TE4R2PYJPO/view?usp=sharing",
+    "https://drive.google.com/file/d/1ypGdf3GHuqdKxQzG1Gc8wEjJ1Pk-TIXc/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/priyanshu-negi-87a56124a/",
   twitter: "https://twitter.com/Priyanshu_1105",
   insta: "https://www.instagram.com/_priyansh_u123/",
